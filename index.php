51e5ceb917ed26ee5826c9a169d40e1f539fe929
d.chantreau <chantreaudamien@gmail.com>
2021-10-24 15:02:18 +0200

    update file index.html with the cow who says hello and a few files more

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php 
+require 'vendor/autoload.php';
 
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+
+$output = $bessie->say();
+echo $output;
+
+$bessie = new Cow('Hello, Farm!');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
+?>
